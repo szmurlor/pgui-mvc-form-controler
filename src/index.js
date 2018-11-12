b0ca61f74d4bf66523a7a127e14ba07e045e199c
Robert Szmurło <robert.szmurlo@ee.pw.edu.pl>
2018-11-12 15:02:37 +0100

    Dodalismy style, kolorujemy, w konstruktorze stan, ale...
    NIestety się nie odswieza 'stan' po zmianie kontroli.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,66 @@ import "./styles.css";
 
 import { DataSet } from "./data.js";
 
+class Station extends React.Component {
+  constructor(props) {
+    super(props);
+    let s = props.station;
+    this.state = {
+      variance: s.expected - s.value,
+      value: s.value,
+      color: s.value > s.expected ? "critical" : "auto"
+    };
+  }
+
+  render() {
+    var s = this.props.station;
+    return (
+      <div className="details">
+        <form>
+          <ul>
+            <li>
+              <span>Identyfikator:</span>
+              <span>
+                <input type="text" readOnly value={s.name} />
+              </span>
+            </li>
+            <li>
+              <span>Data pomiaru:</span>
+              <span>
+                <input type="text" readOnly value={s.date} />
+              </span>
+            </li>
+            <li>
+              <span>Oczekiwana:</span>
+              <span>
+                <input type="text" readOnly value={s.expected} />
+              </span>
+            </li>
+            <li>
+              <span>Zmierzona:</span>
+              <span>
+                <input type="text" value={s.value} value={this.state.value} />
+              </span>
+            </li>
+            <li>
+              <span>Różnica:</span>
+              <span>
+                <input
+                  type="text"
+                  readOnly
+                  className={this.state.color}
+                  readOnly
+                  value={this.state.variance}
+                />
+              </span>
+            </li>
+          </ul>
+        </form>
+      </div>
+    );
+  }
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -14,11 +74,31 @@ class App extends React.Component {
     };
   }
 
+  selected = e => {
+    var selected = undefined;
+    var k = e.target.value;
+    for (var idx in this.state.data.stations) {
+      var s = this.state.data.stations[idx];
+      if (s.id == parseInt(k)) {
+        selected = s;
+        break;
+      }
+    }
+    this.setState({
+      selected: selected
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <div className="left">
-          <select className="stations" name="stations" multiple>
+          <select
+            className="stations"
+            name="stations"
+            onChange={this.selected}
+            multiple
+          >
             {this.state.data.stations.map(s => {
               return (
                 <option key={s.id} value={s.id}>
@@ -28,7 +108,9 @@ class App extends React.Component {
             })}
           </select>
         </div>
-        <div className="right">Tutaj będzie nasza stacja</div>
+        <div className="right">
+          {this.state.selected && <Station station={this.state.selected} />}
+        </div>
       </div>
     );
   }
